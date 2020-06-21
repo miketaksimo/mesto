ef89a163391c57f2193d0369871946b07cfcf06d
Михаил Ермаченко <ermaki72@gmail.com>
2020-06-21 19:08:18 +0700

    Сверстать сайт

---
+++ scripts/popup.js
@@ -0,0 +1,26 @@
+const popup = document.querySelector('.popup')
+const popupOpenButton = document.querySelector('.profile__edit-button')
+const popupCloseButton = popup.querySelector('.popup__close')
+const popupCover = popup.querySelector('.popup__cover')
+const formElement = document.querySelector('.popup__forms')
+let profileName = document.querySelector('.profile__title')
+let profileJob = document.querySelector('.profile__subtitle')
+let nameInput = formElement.querySelector('.popup__input_type_name')
+let jobInput = formElement.querySelector('.popup__input_type_job')
+
+const popupToggle = function() {
+    popup.classList.toggle('popup_open')
+}
+
+popupOpenButton.addEventListener('click', popupToggle)
+popupCloseButton.addEventListener('click', popupToggle)
+popupCover.addEventListener('click', popupToggle)
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+    popupToggle();
+}
+
+formElement.addEventListener('submit', formSubmitHandler)
