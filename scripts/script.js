a7665bdb915e39aa0b55c4db93c56667af5c4658
Михаил Ермаченко <ermaki72@gmail.com>
2020-07-09 00:22:28 +0700

    Сделать проект на проверку (fix 08.07.2020)

--- scripts/script.js
+++ scripts/script.js
@@ -3,18 +3,18 @@ const popupEdit = document.querySelector('.popup_edit');
 const popupEditOpenButton = document.querySelector('.profile__edit-button');
 const popupEditCloseButton = document.querySelector('.popup__close_edit');
 const popupEditForms = document.querySelector('.popup__forms_edit');
-let profileName = document.querySelector('.profile__title');
-let profileJob = document.querySelector('.profile__subtitle');
-let popupInputEditname = document.querySelector('.popup__input_edit_name')
-let popupInputEditjob = document.querySelector('.popup__input_edit_job')
+const profileName = document.querySelector('.profile__title');
+const profileJob = document.querySelector('.profile__subtitle');
+const popupInputEditname = document.querySelector('.popup__input_edit_name')
+const popupInputEditjob = document.querySelector('.popup__input_edit_job')
 
 //Добавление картинок
 const popupAdd = document.querySelector('.popup_add');
 const popupAddOpenButton = document.querySelector('.profile__add-button');
 const popupAddCloseButton = document.querySelector('.popup__close_add');
 const popupAddForms = document.querySelector('.popup__forms_add');
-let popupInputAddName = document.querySelector('.popup__input_add_name');
-let popupInputAddUrl = document.querySelector('.popup__input_add_url');
+const popupInputAddName = document.querySelector('.popup__input_add_name');
+const popupInputAddUrl = document.querySelector('.popup__input_add_url');
 const elementsContainer = document.querySelector('.elements');
 const elementTemplate = document.querySelector('.element-template');
 
@@ -22,40 +22,27 @@ const elementTemplate = document.querySelector('.element-template');
 const popupImage = document.querySelector('.popup_image');
 const popupImageCloseButton = document.querySelector('.popup__close_image');
 
-//Добавление слушителя на клики
-popupEditOpenButton.addEventListener('click', handlerEditPopup);
-popupEditCloseButton.addEventListener('click', handlerEditPopup);
-popupEditForms.addEventListener('submit', formEditSubmitHandler);
-popupAddOpenButton.addEventListener('click', handlerAddPopup);
-popupAddCloseButton.addEventListener('click', handlerAddPopup);
-popupAddForms.addEventListener('submit', formsAddSubmitHandler);
-
 function popupToggle(popup) {
   popup.classList.toggle('popup_open');
 }
+
 //Попап изменения данных
-function handlerEditPopup() {
-  if (!popupEdit.classList.contains('popup_open')) {    
-    popupInputEditname.value = profileName.textContent;
-    popupInputEditjob.value = profileJob.textContent;
-  }
-  popupToggle(popupEdit);
+function handlerEditPopup() {   
+  popupInputElName.value = profileName.textContent;
+  popupInputElJob.value = profileJob.textContent;
 }
 
-function formEditSubmitHandler(evt) {
+function formsEditSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = popupInputEditname.value;
   profileJob.textContent = popupInputEditjob.value;
-  handlerEditPopup();
+  popupToggle(popupEdit);
 }
 
 //Попап добавления картинок
 function handlerAddPopup() {
-  if (!popupAdd.classList.contains('popup_open')) {    
-    popupInputAddName.value  = '';
-    popupInputAddUrl.value = '';
-  }
-  popupToggle(popupAdd);
+  popupInputAddName.value  = '';
+  popupInputAddUrl.value = '';
 }
 
 //Попап просмотра картинки
@@ -116,35 +103,24 @@ function formsAddSubmitHandler(evt) {
 
     makeElement(item);
 
-    handlerAddPopup();
+    popupToggle(popupAdd);
 }
 
-//Заготовки
-const initialElements = [
-	{
-			name: 'Архыз',
-			link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-	},
-	{
-			name: 'Челябинская область',
-			link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-	},
-	{
-			name: 'Иваново',
-			link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-	},
-	{
-			name: 'Камчатка',
-			link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-	},
-	{
-			name: 'Холмогорский район',
-			link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-	},
-	{
-			name: 'Байкал',
-			link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-	}
-];
-
-initialElements.forEach(makeElement);
+//Добавление слушителя на клики
+popupEditOpenButton.addEventListener('click', function() {
+  handlerEditPopup;
+  formsEditSubmitHandler;
+  popupToggle(popupEdit);
+});
+popupEditCloseButton.addEventListener('click', function() {
+  popupToggle(popupEdit);
+});
+popupEditForms.addEventListener('submit', formsEditSubmitHandler);
+popupAddOpenButton.addEventListener('click', function() {
+  popupToggle(popupAdd);
+  handlerAddPopup;
+});
+popupAddCloseButton.addEventListener('click', function() {
+  popupToggle(popupAdd);
+});
+popupAddForms.addEventListener('submit', formsAddSubmitHandler);
